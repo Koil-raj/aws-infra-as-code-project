860e7606369107d6ccf070ddc9c1b69b3db9792b
Koil Raj <koilrajjee@gmail.com>
2025-10-19 19:59:06 +0530

    updated VPC settings and local variables

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -3,6 +3,6 @@ locals {
   region      = "eu-central-1"
   zone1       = "eu-central-1a"
   zone2       = "eu-central-1b"
-  eks_name    = "eks-ade"
+  eks_name    = "eks-test"
   eks_version = "1.33"
 }
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -5,6 +5,6 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "ade-dev"
+    Name = "${local.eks_name}"
   }
 }
