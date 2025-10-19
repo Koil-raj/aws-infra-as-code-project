3b979fe94f296e713eca2652706092758dc28f98
Koil Raj <koilrajjee@gmail.com>
2025-10-19 22:23:35 +0530

    updated backend bucket

--- vpc/providers.tf
+++ vpc/providers.tf
@@ -5,15 +5,6 @@ provider "aws" {
 terraform {
   required_version = ">= 1.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.53"
-    }
-  }
-}
-
-terraform {
   backend "s3" {
     bucket         = "eks-terraform-state-bucket-koil"
     key            = "backend-tf-folder/VPC.tfstate"
@@ -22,4 +13,12 @@ terraform {
     use_lockfile   = true
     dynamodb_table = "terraform-state-lock"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.53"
+    }
+  }
 }
+
